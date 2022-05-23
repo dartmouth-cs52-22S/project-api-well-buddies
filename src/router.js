f1c13d83edde3046e96c090d1359621354068b49
lizfrey <elizabeth.w.frey.24@dartmouth.edu>
2022-05-22 23:16:44 -0400

    added in scaffolding

--- src/router.js
+++ src/router.js
@@ -1,61 +1,12 @@
 import { Router } from 'express';
-import * as Posts from './controllers/post_controller';
 import * as UserController from './controllers/user_controller';
 import { requireAuth, requireSignin } from './services/passport';
 
 const router = Router();
 
 router.get('/', (req, res) => {
-  res.json({ message: 'welcome to our blog api!' });
+  res.json({ message: 'welcome to well buddies api' });
 });
-
-// POST `/posts`: `Posts.createPost`
-// GET `/posts`: `Posts.getPosts`
-
-router.route('/posts')
-  .post(requireAuth, async (req, res) => {
-    const posts = await Posts.createPost(req.body, req.user);
-    res.json(posts);
-  })
-  .get(async (req, res) => {
-    let posts = {};
-    if ('keyword' in req.query) {
-      posts = await Posts.search(req.query.keyword);
-    } else {
-      posts = await Posts.getPosts();
-    }
-    res.json(posts);
-  });
-
-// GET `/posts/:id`: `Posts.getPost`
-// PUT `/posts/:id`: `Posts.updatePost`
-// DELETE `/posts/:id`: `Posts.deletePost`
-
-router.route('/posts/:id')
-  .put(requireAuth, async (req, res) => {
-    const updatedPost = await Posts.updatePost(req.params.id, req.body);
-    res.json(updatedPost);
-  })
-  .patch(requireAuth, async (req, res) => {
-    const updatedPost = await Posts.updatePost(req.params.id, req.body);
-    res.json(updatedPost);
-  })
-  .get(async (req, res) => {
-    try {
-      const post = await Posts.getPost(req.params.id);
-      res.json(post);
-    } catch (error) {
-      res.status(404).json({ error });
-    }
-  })
-  .delete(requireAuth, async (req, res) => {
-    try {
-      const confirmation = await Posts.deletePost(req.params.id);
-      res.json(confirmation);
-    } catch (error) {
-      res.status(404).json({ error });
-    }
-  });
 
 router.post('/signin', requireSignin, async (req, res) => {
   try {
