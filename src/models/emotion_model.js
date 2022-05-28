564893568930c19acd0439808c50d96231ba8fe6
Alina Chadwick <alina.chadwick@gmail.com>
2022-05-28 16:22:14 -0400

    added basic emotion scaffolding

---
+++ src/models/emotion_model.js
@@ -0,0 +1,15 @@
+import mongoose, { Schema } from 'mongoose';
+
+const emotionSchema = new Schema(
+  {
+    title: String,
+    user: { type: Schema.Types.ObjectId, ref: 'User' },  //not sure if this is necessary
+    //date
+
+  },
+
+);
+
+const emotionModel = mongoose.model('Emotion', emotionSchema);
+
+export default emotionModel;
