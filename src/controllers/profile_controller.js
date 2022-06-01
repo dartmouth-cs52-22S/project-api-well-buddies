1d679c5bf055c6d86d4c0fd2d9d153625c381d46
lizfrey <elizabeth.w.frey.24@dartmouth.edu>
2022-06-01 05:16:15 -0400

    fixed set buddy

--- src/controllers/profile_controller.js
+++ src/controllers/profile_controller.js
@@ -64,14 +64,13 @@ export async function getBuddy(jwtToken) {
   }
 }
 
-export async function setBuddy(jwtToken, data) {
+export async function setBuddy(data) {
   try {
-    const email = jwt.decode(jwtToken, process.env.AUTH_SECRET);
+    const email = jwt.decode(data.token, process.env.AUTH_SECRET);
     const foundUser = await Profile.findOne({ email });
     if (foundUser === null) {
       throw new Error('Buddy not found');
     }
-    console.log('data', data);
     if (data.pet !== '') {
       foundUser.pet = data.pet;
     }
